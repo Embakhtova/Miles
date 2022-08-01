89d9ecc38fa31b52ae38a469e6630ec8acbb76f3
Дарья Ембахтова <embakhtovada@gmail.com>
2022-08-02 00:23:22 +0500

    miles

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 1000;
+        int bonus = 20;
+        int miles = ticket / bonus;
+
+        System.out.println("Количество миль при покупке билета за 1000р:" + miles);
+    }
+}
+
