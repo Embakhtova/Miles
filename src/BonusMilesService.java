e120d0906f191419c76c9302d3626f12462eed93
Дарья Ембахтова <embakhtovada@gmail.com>
2022-08-06 12:49:51 +0500

    miles2

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int price = cost;
+        int bonus = 20;
+        return price/bonus;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 1000;
-        int bonus = 20;
-        int miles = ticket / bonus;
-
-        System.out.println("Количество миль при покупке билета за 1000р:" + miles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
 
